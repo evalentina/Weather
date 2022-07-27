f772123e8f04f90499e312939a2e56c4b386946a
Valentina Evdokimova <evdokimovalentina00@gmail.com>
2022-07-27 20:32:20 +0300

    Add labels

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+        
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
