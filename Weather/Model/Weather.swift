dbc9525826aaed8c370091b73b64a5aa67c8b006
Valentina Evdokimova <evdokimovalentina00@gmail.com>
2022-07-30 14:12:02 +0300

    Add model Weather

---
+++ Weather/Model/Weather.swift
@@ -0,0 +1,47 @@
+//
+//  Weather.swift
+//  Weather
+//
+//  Created by Валентина Евдокимова on 30.07.2022.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let cityName: String
+    
+    let temperature: Double
+    var temperatureString: String {
+        return String(format: "%.1f", temperature)
+    }
+    
+    let feelsLikeTemperature: Double
+    var feelsLikeTemperatureString: String {
+        return String(format: "%.1f", feelsLikeTemperature)
+    }
+    
+    let conditionCode: Int
+    var systemIconNameString: String {
+        switch conditionCode {
+        case 200...232: return "cloud.bolt.rain.fill"
+        case 300...321: return "cloud.drizzle.fill"
+        case 500...531: return "cloud.rain.fill"
+        case 600...622: return "cloud.snow.fill"
+        case 701...781: return "smoke.fill"
+        case 800: return "sun.min.fill"
+        case 801...804: return "cloud.fill"
+        default: return "nosign"
+        }
+    }
+    
+    let description : String
+    
+    init?(currentWeatherData: WeatherData) {
+        cityName = currentWeatherData.name
+        description = currentWeatherData.weather.description
+        temperature = currentWeatherData.main.temp
+        feelsLikeTemperature = currentWeatherData.main.feelsLike
+        conditionCode = currentWeatherData.weather.first!.id
+    }
+}
+
