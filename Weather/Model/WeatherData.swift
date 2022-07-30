925488c30b4ea931bcfbfb4adffc08adb4fca9e6
Valentina Evdokimova <evdokimovalentina00@gmail.com>
2022-07-30 14:07:36 +0300

    Add model Weather Data

---
+++ Weather/Model/WeatherData.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherData.swift
+//  Weather
+//
+//  Created by Валентина Евдокимова on 30.07.2022.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+    }
+}
+
+struct Weather: Codable {
+    let id: Int
+    let description : String
+}
